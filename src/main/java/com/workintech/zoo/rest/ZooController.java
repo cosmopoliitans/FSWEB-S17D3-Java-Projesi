118e6d635932bc5e946fe48244116da8639bfe4f
cosmopoliitans <elfkyn52@gmail.com>
2023-08-27 18:12:04 +0300

    Server running

---
+++ src/main/java/com/workintech/zoo/rest/ZooController.java
@@ -0,0 +1,9 @@
+package com.workintech.zoo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zoo")
+public class ZooController {
+}
